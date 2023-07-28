a8dc1494bad51ef5c0b86cffe7799171722e457d
Yuri Batistela <yuribatistela@gmail.com>
2023-07-27 23:57:27 -0300

    Refactoring env config modulo

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -5,6 +5,7 @@ import { ConfigService } from '@nestjs/config'
 @Injectable()
 export class EnvConfigService implements EnvConfig {
   constructor(private configService: ConfigService) {}
+
   getAppPort(): number {
     return Number(this.configService.get<number>('PORT'))
   }
