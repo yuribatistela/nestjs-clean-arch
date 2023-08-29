1d6135c14c09551f352d70f1b0eaacbdac37b49f
Yuri Batistela <yuribatistela@gmail.com>
2023-08-28 22:53:26 -0300

    Abstract class entity - unit tests

--- src/shared/domain/entities/__tests__/unit/entity.spec.ts
+++ src/shared/domain/entities/__tests__/unit/entity.spec.ts
@@ -14,5 +14,27 @@ describe('UserEntity unit tests', () => {
     const entity = new StubEntity(props)
 
     expect(entity.props).toStrictEqual(props)
+    expect(entity._id).not.toBeNull()
+    expect(uuidValidate(entity._id)).toBeTruthy()
+  })
+
+  it('Should accept a valid uuid', () => {
+    const props = { prop1: 'Value1', prop2: 15 }
+    const id = '7db84f85-7399-4628-ab1b-8dd5981c2360'
+    const entity = new StubEntity(props, id)
+
+    expect(uuidValidate(entity._id)).toBeTruthy()
+    expect(entity._id).toBe(id)
+  })
+
+  it('Should convert a entity to a javascript object', () => {
+    const props = { prop1: 'Value1', prop2: 15 }
+    const id = '7db84f85-7399-4628-ab1b-8dd5981c2360'
+    const entity = new StubEntity(props, id)
+
+    expect(entity.toJSON()).toStrictEqual({
+      id,
+      ...props,
+    })
   })
 })
